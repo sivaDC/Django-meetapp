8061a60849e66dccd43b0bfd4ec1a5c721f770a8
Siva DC <sivaaquaris.007@gmail.com>
2022-10-04 23:49:49 +0530

    static

--- meetapp/settings.py
+++ meetapp/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_URL = "/dynamic/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
