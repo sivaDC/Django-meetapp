c6368e4f6d0a33a11374edbfcaa602d345aee79e
Siva DC <sivaaquaris.007@gmail.com>
2022-10-04 23:50:13 +0530

    admin

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Meetup, Location
+
+class MeetupAdmin(admin.ModelAdmin):
+    list_display = ('title','slug')
+    list_filter = ('location',)
+    prepopulated_fields = {'slug':('title',)}
+
+admin.site.register(Meetup,MeetupAdmin)
+admin.site.register(Location)
--- meetups/views.py
+++ meetups/views.py
@@ -1,32 +1,30 @@
+from ast import Try
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Meetup
+
 # Create your views here.
 def Index(request):
-    meetups = [
-   {  'title': 'A First Meetup' ,
-        'location' : 'New York' ,
-        'slug' : 'a-first-meetup'
-    } ,
-   {
-        'title': 'A Second Meetup' ,
-        'location' : 'Paris' ,
-        'slug' : 'a-second-meeetup'
-   }
-]
-       
+    meetups = Meetup.objects.all()
     return render(request,"meetups/index.html",{
-        'show_meetups':False,
         'meetups':meetups
         })
 
 
 def meetup_details (request,meetup_slug) :
-    selected_meetup = {
-        'title' : ' A First Meetup ' ,
-        'description' : ' This is the first meetup ! '
-       }
-    return render ( request , 'meetups/meetup-detail.html' , {
-        'meetup_title' : selected_meetup['title'] ,
-        'meetup_description' : selected_meetup['description']
-   } )
+    try:
+        selected_meetup = Meetup.objects.get(slug=meetup_slug)
+        return render ( request , 'meetups/meetup-detail.html' , {
+            'meetup_found' : True ,
+            'meetup_title' : selected_meetup.title,
+            'meetup_description' : selected_meetup.description,
+            'meetup_img': selected_meetup.image.url,
+            'meetup_city': selected_meetup.location.name,
+            'meetup_addr': selected_meetup.location.address,
+
+         })
+    except Exception as exe:
+        return render ( request , 'meetups/meetup-detail.html' , {
+            'meetup_found' : False ,
+         })
